62ba7c12bca3d3fa08d678eb83914888207ee70d
Dana Hazen <danahazen79@gmail.com>
2015-11-11 16:04:38 -0600

    ordered lists

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,23 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("WelcomeController", ["$scope", "$http", function($scope, $http){
+myApp.controller("WelcomeController", ["$scope", "$http", '$filter', function($scope, $http, $filter){
     $scope.info = {};
-    $scope.people = [];
+    $scope.people = [{name: "Dana", location: "Brooklyn Park", fav_num: 42}, {name: "Sam Moss", location: "Minneapolis", fav_num: "NaN"}];
+    var orderBy = $filter('orderBy');
 
-    $scope.getPeople = function(){
-        $http.get('/info').then(function(response){
-            $scope.people = response.data.rows;
-            console.log($scope.people);
-        });
+    //$scope.getPeople = function(){
+    //    $http.get('/info').then(function(response){
+    //        $scope.people = response.data.rows;
+    //        console.log($scope.people);
+    //    });
+    //};
+
+    $scope.order = function(predicate, reverse) {
+        $scope.people = orderBy($scope.people, predicate, reverse);
     };
 
-    $scope.getPeople();
+    //$scope.getPeople();
+    $scope.order();
 
 }]);
 
--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var pg = require('pg');
+//
 
-var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/wk5_angular";
+//var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/wk5_angular";
 
 app.set("port", process.env.PORT || 5000);
 
-app.get('/info', function(req,res){
-    pg.connect(connectionString, function(err, client){
-        client.query("SELECT * FROM info ",
-            function (err,result){
-                if (err) {
-                    console.log("Error retrieving data: ", err);
-                    res.send(false);
-                }
-                console.log(result);
-                res.send(result);
-            }
-        )
-    })
-});
+//app.get('/info', function(req,res){
+//    pg.connect(connectionString, function(err, client){
+//        client.query("SELECT * FROM info ",
+//            function (err,result){
+//                if (err) {
+//                    console.log("Error retrieving data: ", err);
+//                    res.send(false);
+//                }
+//                console.log(result);
+//                res.send(result);
+//            }
+//        )
+//    })
+//});
 
 app.get("/*", function(req,res){
     var file = req.params[0] || "assets/views/index.html";
